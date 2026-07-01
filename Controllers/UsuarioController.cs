1d66fc55e3f792a381cfdf5622034aa4a8d176c9
KappaThrash <danielkuhin@gmail.com>
2026-07-01 02:08:41 -0300

    ajustes no envio de imagem do usuario, ainda irei tirar essas verificacoes do controller para uma classe separada.

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using PicPay.Domains;
 using PicPay.Services.UsuarioServices;
@@ -12,11 +11,19 @@ namespace PicPay.Controllers
 
         private readonly ILogger<UsuarioController> _logger;
         private readonly IUsuarioService _usuarioService;
+        private readonly IConfiguration _configuration;
+        private readonly long _maxFileSize;
+        private readonly IReadOnlyCollection<string> _allowedImageTypes;
+        private readonly IReadOnlyCollection<string> _allowedImageExtensions;
 
-        public UsuarioController(ILogger<UsuarioController> logger, IUsuarioService usuarioService)
+        public UsuarioController(ILogger<UsuarioController> logger, IUsuarioService usuarioService, IConfiguration configuration)
         {
             _logger = logger;
             _usuarioService = usuarioService;
+            _configuration = configuration;
+            _maxFileSize = _configuration.GetValue<long>("ImageSettings:MaxFileSize");
+            _allowedImageTypes = _configuration.GetSection("ImageSettings:AllowedImageTypes").Get<IReadOnlyCollection<string>>()!;
+            _allowedImageExtensions = _configuration.GetSection("ImageSettings:AllowedImageExtensions").Get<IReadOnlyCollection<string>>()!;
         }
 
         [HttpPost]
@@ -30,14 +37,26 @@ namespace PicPay.Controllers
         [HttpPost("{id}/imagem")]
         public async Task<ActionResult> PostUsuarioImagem([FromForm] IFormFile file, Guid id)
         {
-            if (file.Length <= 0 || file == null)
+            string extension = Path.GetExtension(file.FileName).ToLowerInvariant();
+
+            if (file == null || file.Length <= 0)
             {
                 return BadRequest("Arquivo tem tamanho 0 ou não enviado");
             }
 
-            if (!file.ContentType.StartsWith("image/"))
+            if (file.Length > _maxFileSize)
             {
-                return BadRequest("Arquivo não é uma imagem");
+                return StatusCode(StatusCodes.Status413PayloadTooLarge, "Arquivo passou do limite (10Mb)");
+            }
+
+            if (!file.ContentType.StartsWith("image/") || !_allowedImageTypes.Contains(file.ContentType))
+            {
+                return StatusCode(StatusCodes.Status415UnsupportedMediaType, "Tipo de arquivo não suportado para esse endpoint");
+            }
+            
+            if (!_allowedImageExtensions.Contains(extension))
+            {
+                return StatusCode(StatusCodes.Status415UnsupportedMediaType, "Extensão de arquivo não suportada para esse endpoint");
             }
 
             var usuario = await _usuarioService.SaveUsuarioImagemAsync(file, id);
@@ -48,7 +67,12 @@ namespace PicPay.Controllers
         public async Task<ActionResult<Usuario>> GetUsuario(Guid id)
         {
             var usuario = await _usuarioService.FindUsuarioAsync(id);
-            
+
+            if(usuario == null)
+            {
+                return NotFound($"Usuario não encontrado {id}");
+            }
+
             return Ok(usuario);
         }
     }
--- tests/PicPay.Tests/UsuarioServiceTests.cs
+++ tests/PicPay.Tests/UsuarioServiceTests.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Moq;
+using PicPay.Domains;
+using PicPay.Factory;
+using PicPay.Repository.UsuarioRepositories;
+using PicPay.Services.UsuarioServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +12,34 @@ using System.Threading.Tasks;
 
 namespace PicPay.Tests
 {
-    internal class UsuarioServiceTests
+    public class UsuarioServiceTests
     {
+        private readonly UsuarioService _service;
+        private readonly Mock<IUsuarioRepository> usuarioRepository;
+        private readonly Usuario usuarioTest;
+        //private readonly IFormFile file;
 
+        public UsuarioServiceTests()
+        {
+           usuarioRepository = new Mock<IUsuarioRepository>();
+
+           _service = new UsuarioService(usuarioRepository.Object);
+
+            var usuario = UsuarioFactory.UsuarioLojista();
+
+            //using var stream = new MemoryStream();
+            //file = new FormFile(stream, 0,stream.Length,"formtest","filetest");
+        }
+
+        /*[Fact]
+        public async Task SaveUsuarioAsyncTest()
+        {
+            var dto = new UsuarioDTO(); 
+
+            await _service.SaveUsuarioAsync(dto);
+
+            usuarioRepository.Setup(x => x.SaveAsync(usuarioTest))
+                .Returns(Task.FromResult(usuarioTest));
+        }*/
     }
 }
